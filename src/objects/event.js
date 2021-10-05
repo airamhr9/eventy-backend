fdb9871f2ec7f4eb424169e076eab6624065371f
Rubén <rubenlopezrosique@gmail.com>
2021-10-05 17:59:59 +0200

    class Event

---
+++ src/objects/event.js
@@ -0,0 +1,17 @@
+class Event {
+    construtor(id, name, startDate, finishDate, location, description, summary, private, images, maxParticipants, price, owner, chat){
+        this.id = id
+        this.name = name
+        this.startDate = startDate
+        this.finishDate = finishDate
+        this.location = location
+        this.description = description
+        this.summary = summary
+        this.private = private
+        this.images = images
+        this.maxParticipants = maxParticipants
+        this.price = price
+        this.owner = owner
+        this.chat = chat
+    }
+}
