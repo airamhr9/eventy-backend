addcabb6e81f233d21f0750420065c853ad3b189
David Hernández Rodrigo <daherro3@upv.edu.es>
2021-10-06 19:13:47 +0200

    busqueda (sin tags)

--- src/search/search.js
+++ src/search/search.js
@@ -1,18 +1,34 @@
-import { child, DataSnapshot, get, ref } from '@firebase/database'
+import { child, get, ref } from '@firebase/database'
 import { rdb } from '../index.js'
 
 const rdbRef = ref(rdb)
 
-get(child(rdbRef, "events")).then((snapshot) => {
-    if (snapshot.exists()) {
-      console.log(snapshot.val());
-    } else {
-      console.log("No data available");
-    }
-  }).catch((error) => {
-    console.error(error);
-  });
+function search(searchText, searchTags){
 
-/*function search(searchText, searchTags){
-    
-}*/
+    get(child(rdbRef, 'events/')).then((snapshot) =>{
+      if(snapshot.exists()){
+        let result = []
+        let events = snapshot.val()
+
+        events.forEach(element => {
+          let names = makeLowerCase(element.name)
+
+          if(names.includes(makeLowerCase(searchText))){
+            result.push(element)
+          }
+        })
+
+        returnEvents(result)
+      }
+    })
+}
+
+search('DE', [])
+
+function makeLowerCase(value) {
+  return value.toString().toLowerCase();
+}
+
+function returnEvents(result){
+  console.log(result)
+}
