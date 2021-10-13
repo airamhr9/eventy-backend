3840075fc08c20c7462727a6f780acde31df610c
airamhr9 <airamhr7@gmail.com>
2021-10-13 20:35:37 +0200

    error con private en publish.js

--- src/events/publish.js
+++ src/events/publish.js
@@ -6,7 +6,7 @@ const app = express()
 const dbm = new DatabaseManager
 
 
-export function publishEvent(description, finishDate, images, location, maxParticipants, name, owner, price, private, startDate, summary, tags){ 
+export function publishEvent(description, finishDate, images, location, maxParticipants, name, owner, price, isPrivate, startDate, summary, tags){ 
     const newEvent = new Event( description, 
                                 finishDate, 
                                 images, 
@@ -15,7 +15,7 @@ export function publishEvent(description, finishDate, images, location, maxParti
                                 name, 
                                 owner, 
                                 price, 
-                                private, 
+                                isPrivate, 
                                 startDate,
                                 summary,
                                 tags,)
