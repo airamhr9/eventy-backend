bfa7118c71e6e9ef107b93483dbb8078f12125e5
Rubén <rubenlopezrosique@gmail.com>
2021-10-14 18:03:12 +0200

    login 0.5

---
+++ src/users/login.js
@@ -0,0 +1,10 @@
+import { DatabaseManager } from '../database/databaseManager.js'
+
+const dbm = new DatabaseManager
+
+print(dbm.getUserPasswordById("antoniogabinete"))
+
+export function login(userId, password){ 
+        
+        return true
+    }
