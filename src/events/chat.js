a5dc3c19844462091eb6e4dbdc5caffa48c126ad
David Hernández Rodrigo <daherro3@upv.edu.es>
2021-10-18 20:26:30 +0200

    chat start

--- src/events/chat.js
+++ src/events/chat.js
@@ -1,7 +1,25 @@
-import { ref } from '@firebase/database'
+import { get, ref } from '@firebase/database'
 import {rdb} from '../index.js'
 
 
-export function eventChat(eventId){
-    
+
+export function eventChat(res, eventId){
+    const refRdb = ref(rdb, `events/${eventId}/messages`)
+
+    get(child(rdb, `events/${eventId}/messages`)).then((snapshot =>{
+
+
+    }))
+
+}
+
+export function sendMssg(userId, text, eventId){
+    const refRdb = ref(rdb, `events/${eventId}/messages`)
+
+    let mssg = Message(userId, text, Date.now())
+
+    refRdb.set({
+        mssg
+    })
+
 }
---
+++ src/objects/message.js
@@ -0,0 +1,7 @@
+class Message{
+    constructor(user, text, time){
+        this.user = user,
+        this.text = text,
+        this.time = time
+    }
+}
