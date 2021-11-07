8c6c2d8f29a38991e536212fa2e2276778a8b928
David Hernández Rodrigo <daherro3@upv.edu.es>
2021-11-07 20:38:12 +0100

    filtro con +- distancia

--- src/search/search.js
+++ src/search/search.js
@@ -57,17 +57,30 @@ function filter(filters, searchedEvents, res){
     let loc = [element.latitude, element.longitude]
 
     if(filters[0] != false && new Date(filters[1]).getDate() == sDate && sDate == fDate){
-      if((filters[3] >= pr || filters[3] == undefined) && (filters[4] == loc || filters[4] == undefined)){
-        resultFilter.push(element)
+      if(filters[3] >= pr || filters[3] == ""){
+        if(plusMinus(filters[4], loc, 0.2) != false || filters[4] == ""){
+          resultFilter.push(element)
+        }
       }
     }
     else if(filters[0] != true && new Date(filters[1]).getDate() == sDate && new Date(filters[2]).getDate() == fDate && sDate != fDate){
-      if((filters[3] >= pr || filters[3] == "") && (filters[4] == loc || filters[4] == "")){
-        resultFilter.push(element)
+      if(filters[3] >= pr || filters[3] == ""){
+        if(plusMinus(filters[4], loc, 0.2) != false || filters[4] == ""){
+          resultFilter.push(element)
+        }
       }
     }
   })
   returnEvents(res, resultFilter)
+}
+
+function plusMinus(n1, n2, diff){
+  if (Math.abs(n1[0] - n2[0]) <= diff && Math.abs(n1[1] - n2[1]) <= diff) {
+    return true
+  }
+  else{
+    return false
+  }
 }
 
 async function returnEvents(res, events){
