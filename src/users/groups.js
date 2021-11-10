3fe6540bc2cca4cb5ffe2ca286f55b8d16bf41ac
Hector Alcaide <hecalrol@outlook.com>
2021-11-10 10:16:12 +0100

    Obtener grupos de un usuario

---
+++ src/users/groups.js
@@ -0,0 +1,42 @@
+import { child, get, ref } from '@firebase/database'
+import { rdb } from '../index.js'
+import { getUser, user } from './userProfile.js'
+import { replaceImagesWithURL_User, objectWithURLs } from '../images.js'
+
+const rdbRef = ref(rdb)
+
+export function getUserGroups(userId, res) {
+    get(child(rdbRef, 'groups')).then(async (snapshot) => {
+        let result = []
+        if (snapshot.exists()) {
+            let groups = snapshot.val()            
+            groups = Object.values(groups)
+            for (let g of groups) {
+                let groupUsersIds = Object.keys(g.users)
+                if (groupUsersIds.includes(userId)) {
+                    let objectToSend = {}
+                    objectToSend.id = g.id
+                    objectToSend.users = []
+                    // Añadir nombre y foto a cada usuario para enviar
+                    for (let u of Object.values(g.users)) {
+                        await getUser(u.userId)
+                        await replaceImagesWithURL_User(user)
+                        objectToSend.users.push({
+                            "id": u.userId,
+                            "username": objectWithURLs.username,
+                            "image": objectWithURLs.image,
+                            "dateMin": u.dateMin,
+                            "dateMax": u.dateMax,
+                            "price": u.price,
+                            "tags": u.tags
+                        })
+                    }
+                    result.push(objectToSend)
+                }
+            }
+        }
+        res.send(result)
+    }).catch((error) => {
+        console.error(error)
+    })
+}
