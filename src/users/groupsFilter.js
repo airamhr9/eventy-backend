4d6d3cc9bf92527343c3e5f5c1aad07497a0bcf6
HacheInTheWild <davrodh@gmail.com>
2021-11-17 09:51:13 +0100

    filtros y todo mal hechisimo

--- src/users/groupsFilter.js
+++ src/users/groupsFilter.js
@@ -102,7 +102,16 @@ function recommendToGroups(groupMaxDates, groupMinDates, groupPrices, groupTags,
             let result = filterWithSelected(maxStartDate, minFinishDate, minPrice, foundEvents)
 
             if(result.length > 0){
-                returnEvents(res, result)
+                let joya = true
+                returnEvents(res, result, joya)
+            }
+            else{
+                let resultF = filterWithSelected(maxStartDate, getMinDate(extractMin(groupMinDates)), minPrice, foundEvents)
+                let resultS = filterWithSelected(getMaxDate(extractMax(groupMaxDates)), minFinishDate, minPrice, foundEvents)
+                let resultP = filterWithSelected(maxStartDate, minFinishDate, Math.min.apply(null, extractMin(groupPrices)), foundEvents)
+                let joya = false
+                let combinedRes = [resultS, resultF, resultP]
+                returnEvents(res, combinedRes, joya)
             }
         }
     })
@@ -126,6 +135,35 @@ function getMinDate(arrDates){
     }
 }
 
+function extractMax(arr){
+    try{
+        let index = arr.indexOf(Math.max.apply(null, arr))
+        if(index > -1){
+            arr.splice(index, 1)
+            return arr
+        }
+         
+    }
+    catch(error){
+        return "te jodes pero sin lo más grande"
+    }
+}
+
+function extractMin(arr){
+    try{
+        let index = arr.indexOf(Math.min.apply(null, arr))
+        
+        if(index > -1){
+            arr.splice(index, 1)
+            return arr
+        }
+         
+    }
+    catch(error){
+        return "te jodes pero sin lo más xikito"
+    }
+}
+
 function findCommonElements(arr1, arr2) {
     try {
       return arr1.some(item => arr2.includes(item))
@@ -142,19 +180,35 @@ function filterWithSelected(maxSDate, minFDate, minPrice, eventList){
             filteredEvents.push(eventList[key])            
         }
     }
-
     return filteredEvents
 }
 
-async function returnEvents(res, events){
+async function returnEvents(res, events, todoJoya){
     try {
-      let result = []
-      for (let ev of events) {
-        await replaceImagesWithURL_Event(ev)
-        result.push(objectWithURLs)
-      }
-      res.send(result)
+        if(todoJoya == true){
+            let result = []
+            for (let ev of events) {
+                await replaceImagesWithURL_Event(ev)
+            result.push(objectWithURLs)
+            }
+            let response = {"todo bien": todoJoya, "items":result}
+            res.json(response)
+        }
+        else{
+            let result = [[],[],[]]
+            let i = 0
+            for (let ev of events) {
+                for (const key in ev) {
+                    console.log()
+                    await replaceImagesWithURL_Event(ev[key])
+                    result[i].push(objectWithURLs)
+                }
+                i++
+            }
+            let response = {"todo bien": todoJoya, "items Sin Start Date":result[0], "items Sin Finish Date":result[1], "items Sin Precio":result[2]}
+            res.json(response)
+        }
     } catch (error) {
-      return "no se pudo enviar"
+      return "no se pudo enviar, así que te jodes"
     }
   }
