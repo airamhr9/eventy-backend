d9cc45463b9d2be789ae9d2b337c02ff5ac1799f
Hector Alcaide <hecalrol@outlook.com>
2021-12-05 18:00:25 +0100

    Cambios en recuerdos

--- src/events/memories.js
+++ src/events/memories.js
@@ -1,18 +1,13 @@
-import { child, get, ref, set, push, getDatabase} from '@firebase/database'
-import {sendMemoriesWithImages} from '../images.js'
+import { child, get, ref, set, push } from '@firebase/database'
+import { sendMemoriesWithImages } from '../images.js'
 import { rdb } from '../index.js'
 
 const rdbRef  = ref(rdb)
-const db = getDatabase()
-export let memory
 
-export async function createMemory(eventId, text, author, images, res) {
+export function createMemory(eventId, text, author, images, res) {
     const rdbRefC = ref(rdb,`events/${eventId}/memories/`)
     const newRef = push(rdbRefC)
     const idKey = newRef.key
-
-
-
     set(newRef,
     {   
         id : idKey,
@@ -21,34 +16,21 @@ export async function createMemory(eventId, text, author, images, res) {
         author : author, 
         images : images
 
-    }).catch((error) => {console.error(error)})
+    }).catch((error) => {
+        console.error(error)
+    })
     res.send(idKey)
-
 }
 
-export async function getMemories(eventId, res) {
+export function getMemories(eventId, res) {
     get(child(rdbRef,`events/${eventId}/memories/`)).then((snapshot) => {
-        if(snapshot.exists()){
-            const memories = snapToArray(snapshot)
-            sendMemoriesWithImages(memories,res)
-        }else{
+        if (snapshot.exists()) {
+            const memories = Object.values(snapshot.val())
+            sendMemoriesWithImages(memories, res)
+        } else {
             res.send([])
         }
-    }).catch((error) => {console.error(error)})
-}
-
-
-
-
-function snapToArray(snapshot){
-    var returnArr = []
-
-    snapshot.forEach(function(childSnapshot) {
-        var item = childSnapshot.val()
-        item.key = childSnapshot.key
-
-        returnArr.push(item)
+    }).catch((error) =>{
+        console.error(error)
     })
-
-    return returnArr
 }
