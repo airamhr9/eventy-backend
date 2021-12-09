7e883a9781daa5d69160f9bc663394726646505c
David Hernández Rodrigo <davrodh@gmail.com>
2021-12-09 18:15:25 +0100

    load testing v1

--- test/loadTesting.js
+++ test/loadTesting.js
@@ -1,20 +1,35 @@
 import { expect } from "chai"
 import { loadTest } from 'loadtest'
 
-describe('stress testing', function(){
-    it('acces should be capable of handeling 30 users', function(done){
+describe('load testing --> 30 concurrent users', function(){
+    it('Recommendations (Main Screen)', function(done){
         const options = {
             url: 'http://localhost:8000/recomend?userId=niMeffxvboZAdeimcmoy03Swjfk1&latitude=19&longitude=-2',
             maxRequests: 100,
-            concurrency: 30
+            concurrency: 50
         }
 
         loadTest(options, function(err, result){
             if(err){
                 return console.log(err)
             }
-            console.log(result)
-            expect(result.totalTimeSeconds).to.be.below(1)
+            expect(result.totalTimeSeconds).to.be.below(1) //total time must be below 1 second
+            done()
+        })
+    })
+
+    it('Search', function(done){
+        const options = {
+            url: 'http://localhost:8000/search?text&tags&enabled=false',
+            maxRequests: 100,
+            concurrency: 50
+        }
+
+        loadTest(options, function(err, result){
+            if(err){
+                return console.log(err)
+            }
+            expect(result.totalTimeSeconds).to.be.below(1) //total time must be below 1 second
             done()
         })
     })
